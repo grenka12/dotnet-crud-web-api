a23e2806ad6a6d956dfdba7f0a9d2f3acafd6f3a
Yehor <grenka1313@gmail.com>
2026-01-13 09:25:28 +0000

    tf pipeline file/skelet

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,14 @@ resource "azurerm_service_plan" "sp_dotnet_app" {
   resource_group_name = azurerm_resource_group.rg_main.name
   location            = azurerm_resource_group.rg_main.location
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "P1v2"  
 }
 
 resource "azurerm_linux_web_app" "example" {
   name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+  resource_group_name = azurerm_resource_group.rg_main.name
+  location            = azurerm_service_plan.rg_main.location
+  service_plan_id     = azurerm_service_plan.sp_dotnet_app.id
 
   site_config {}
 }
