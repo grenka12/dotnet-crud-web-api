4b8627a904ba0622b850100010170b6cdc75897e
Yehor <grenka1313@gmail.com>
2026-01-13 09:58:07 +0000

    Fix naming

--- terraform/appservice.tf
+++ terraform/appservice.tf
@@ -6,10 +6,10 @@ resource "azurerm_service_plan" "sp_dotnet_app" {
   sku_name            = "P1v2"  
 }
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "example"
+resource "azurerm_linux_web_app" "wa-main-dotnet" {
+  name                = "Main WebApp Dotnet"
   resource_group_name = azurerm_resource_group.rg_main.name
-  location            = azurerm_service_plan.rg_main.location
+  location            = azurerm_service_plan.sp_dotnet_app.location
   service_plan_id     = azurerm_service_plan.sp_dotnet_app.id
 
   site_config {}
