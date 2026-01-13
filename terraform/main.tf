efe100dee04d9d5da01483c8b3e7fa6a3f557adf
Yehor <grenka1313@gmail.com>
2026-01-13 09:40:39 +0000

    tf test run appservice

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,19 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "dotnet-backend"
+    storage_account_name = "dnbackendtf"
+    container_name       = "tfstate"
+    key                  = "app.terraform.tfstate"
+  }
+}
+
+
 resource "azurerm_resource_group" "rg_main" {
   name     = "dotnet-rg"
   location = "Poland Central"
 }
 
-resource "azurerm_service_plan" "sp_dotnet_app" {
-  name                = "sp_dotnet_app"
-  resource_group_name = azurerm_resource_group.rg_main.name
-  location            = azurerm_resource_group.rg_main.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"  
-}
-
-resource "azurerm_linux_web_app" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.rg_main.name
-  location            = azurerm_service_plan.rg_main.location
-  service_plan_id     = azurerm_service_plan.sp_dotnet_app.id
-
-  site_config {}
-}
+te
