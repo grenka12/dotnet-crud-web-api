7b85fe66172ddd7edeb6fcb4bb382561c010fa9a
Yehor <grenka1313@gmail.com>
2026-01-13 11:28:02 +0000

    change acr name

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr_main" {
-  name                = "acr-dotnetcrud-bestrong"
+  name                = "acrdotnetcrudbestrong"
   resource_group_name = azurerm_resource_group.rg_main.name
   location            = azurerm_resource_group.rg_main.location
 
